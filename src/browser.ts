4510c1da3a9ccc2e62a1b186c6cb1aa0dd7856cb
kato-takeshi <kato-takeshi@mitsue.co.jp>
2020-08-18 13:59:46 +0900

    add view resource.

---
+++ src/browser.ts
@@ -0,0 +1,43 @@
+import path from 'path';
+import express from 'express';
+import webSocket from 'ws';
+import http from 'http';
+
+const projectRoot = path.resolve(__dirname, '..');
+
+// TODO: generate tree data
+
+// TODO: open tree viewer
+
+function startServer() {
+  const app = express();
+
+  app.set('views', `${projectRoot}/views`);
+  app.use(express.static(`${projectRoot}/dist`));
+  app.use('/', (req, res) => {
+    res.render('viewer', {
+      mode: 'server',
+      title: 'title', // TODO: import package.json
+      enableWebSocket: true,
+    });
+  });
+
+
+  const server = http.createServer(app);
+
+  const wss = new webSocket.Server({
+    server,
+  });
+
+  wss.on('connection', (ws) => {
+    ws.on('error', (err) => {
+      console.error(err);
+      // TODO: handling err
+    });
+  });
+
+  return {
+    ws: wss,
+    http: server,
+  };
+}
