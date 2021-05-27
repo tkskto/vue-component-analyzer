1eee1505e4ce6fd208802ce9ac9e4b6bdd7d4737
tkskto <tkskto@gmail.com>
2021-05-27 15:22:17 +0900

    fix: added silent mode to prevent show logs.

--- src/server/index.ts
+++ src/server/index.ts
@@ -30,6 +30,7 @@ function writeFileExtra(filename: string, data: string) {
     program.option('-f, --format [type]', 'Add the specified type of report [browser, json or both]', FORMAT.BROWSER);
     program.option('-o, --out [dir]', 'output directory (enable with setting --format option to "json" or "both")', 'out');
     program.option('-p, --port [number]', 'select a port number for the local server', '8888');
+    program.option('--silent', 'do not show logs with silent flag');
     program.parse(process.argv);
 
     const argv = program.opts();
@@ -38,6 +39,7 @@ function writeFileExtra(filename: string, data: string) {
     model.format = argv.format;
     model.outDirectory = argv.out;
     model.port = argv.port;
+    model.isSilentMode = argv.silent || false;
 
     if (argv.format !== FORMAT.BROWSER && argv.format !== FORMAT.JSON && argv.format !== FORMAT.BOTH) {
       console.error(`not support ${argv.format} format.`);
