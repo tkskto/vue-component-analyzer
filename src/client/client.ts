a3a80b34e664c2ac630f028cdd4b4571cb1c3b47
tkskto <tkskto@gmail.com>
2021-12-12 18:15:09 +0900

    add screen capture.

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,6 +3,7 @@ import {Model} from './model';
 import {Renderer} from './renderer';
 import {setSettings} from './Settings';
 import {setSeedOpenStateSwitcher} from './SeedOpenStateSwitcher';
+import {setScreenCapture} from './ScreenCapture';
 
 const model = new Model();
 new Renderer(model);
@@ -26,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
       model.data = msg; // emit Model.EVENT.DATA_UPDATE event
 
       setSettings(model);
+      setScreenCapture();
     });
   } else {
     console.warn('Couldn\'t connect to analyzer websocket server so you\'ll have to reload page manually to see updates in the treemap');
