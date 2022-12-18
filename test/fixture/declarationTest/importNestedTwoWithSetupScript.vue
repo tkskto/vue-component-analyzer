fde2667aa651df55356d7136f8333696f8a0868d
tkskto <tkskto@gmail.com>
2022-12-18 14:58:33 +0900

    test: update test for script setup, typescript

---
+++ test/fixture/declarationTest/importNestedTwoWithSetupScript.vue
@@ -0,0 +1,7 @@
+<template>
+  <div></div>
+</template>
+<script setup>
+  import nested from '../../components/nested.vue';
+  import nested2 from '../../components/nested2Setup.vue';
+</script>
---
+++ test/fixture/declarationTest/importTwoWithSetupScript.vue
@@ -0,0 +1,7 @@
+<template>
+  <div></div>
+</template>
+<script setup>
+  import child from '../../components/child.vue';
+  import child2 from '../../components/child2.vue';
+</script>
